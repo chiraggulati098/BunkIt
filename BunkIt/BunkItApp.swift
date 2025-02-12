7a00d75957a3262f6d6ceada29b794977d47bc01
Chirag Gulati <chiraggulati098.com>
2025-02-12 20:19:35 +0530

    Initial Commit

---
+++ BunkIt/BunkItApp.swift
@@ -0,0 +1,17 @@
+//
+//  BunkItApp.swift
+//  BunkIt
+//
+//  Created by Chirag Gulati on 12/02/25.
+//
+
+import SwiftUI
+
+@main
+struct BunkItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
